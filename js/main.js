2e68bcda4ddcabd0b9c95717e0643a28ad1fe2a1
Blackstock <charlieblackstock@gmail.com>
2015-08-06 10:45:08 -0600

    added js for rock, paper, scissors test sutie

--- js/main.js
+++ js/main.js
@@ -9,17 +9,36 @@ var Game = function(player1, player2) {
 };
 
 Player.prototype.picks = function (hand) {
+  if (hand === undefined)
+    return this.hand;
   this.hand = hand;
 };
 
 
 Game.prototype.winner = function() {
-    if (this.player1.hand === 'rock' && this.player2.hand === "scissors") {
+  if(this.player1.picks() === this.player2.picks()){
+    return null;
+  }
+  if(this.player1.picks() === 'rock'){
+    if(this.player2.picks() === 'scissors')
       return this.player1;
-    }
+    return this.player2;
+  }
+
+  if(this.player1.picks() === 'scissors'){
+    if(this.player2.picks() === 'rock')
+      return this.player2;
+    return this.player1;
+  }
+  if(this.player1.picks() === 'paper'){
+    if(this.player2.picks() === 'rock')
+      return this.player1;
+    return this.player2;
+  }
+
 };
 
 module.exports = {
   Player: Player,
   Game: Game
-}
+};
