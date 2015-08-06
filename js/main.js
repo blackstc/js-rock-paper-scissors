8a8b3356ae42aadcad710bde359b3ec35b12695f
Blackstock <charlieblackstock@gmail.com>
2015-08-06 09:24:11 -0600

    added OOP for first test suite

--- js/main.js
+++ js/main.js
@@ -2,3 +2,24 @@
 var Player = function(name) {
   this.name = name;
 };
+
+var Game = function(player1, player2) {
+  this.player1 = player1;
+  this.player2 = player2;
+};
+
+Player.prototype.picks = function (hand) {
+  this.hand = hand;
+};
+
+
+Game.prototype.winner = function() {
+    if (this.player1.hand === 'rock' && this.player2.hand === "scissors") {
+      return this.player1;
+    }
+};
+
+module.exports = {
+  Player: Player,
+  Game: Game
+}
